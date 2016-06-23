891f953e5215c3e38fda5b6a0f41a362fd66802a
Danilo Andrés Sandoval Ortiz <BTDASO@fesub.cl>
2016-06-23 16:06:14 -0400

    now compatible with json js object

--- src/jquery.scapo.debug.js
+++ src/jquery.scapo.debug.js
@@ -21,6 +21,16 @@
 $.fn.scapo = function(obj){
     var elem = this;
     var elem_id = elem.prop('id');
+    console.log("---");
+    console.log("This is "+elem_id);
+    console.log("Source is "+obj.source);
+    console.log("Nested is "+obj.nested);
+    if(obj.nested) {
+        console.log("Parent.parent is "+obj.nested.parent);
+        console.log("Parent.child is "+obj.nested.child);
+        console.log("Parent.by is "+obj.nested.by);
+    }
+
     var data;
 
     if(typeof obj.source == 'string') {
@@ -31,7 +41,13 @@ $.fn.scapo = function(obj){
         data = obj.source;
     }
 
+    console.log("Data is "+data);
+
+    console.log("Adding null option to "+elem_id);
+
     var placeholder = (obj.placeholder)?obj.placeholder:'Choose a option ...';
+    
+    console.log("Placeholder is "+placeholder);
 
     $(elem).append($('<option></option>').prop('value',"0").text(placeholder));
 
@@ -47,24 +63,29 @@ $.fn.scapo = function(obj){
             shtml += ">";
             shtml += val[obj.text];
             shtml += "</option";
-
+        console.log("Adding option "+shtml+" to "+elem_id);
         $(elem).append(shtml);
     });
-
     $(elem).on('change',function(event){
         
+        console.log("Trigger Change event for "+elem_id);
+
         if(obj.nested) {
 
             if(obj.nested.parent) {
+                console.log(elem_id+" has parent: "+obj.nested.parent);
                 var eq = $(elem).find(":selected").data(obj.nested.by);
+                console.log("eq is "+eq);
                 $(obj.nested.parent+' option:eq('+eq+')').prop('selected', true);
             }
 
             if(obj.nested.child) {
+                console.log(elem_id+" has child: "+obj.nested.child);
                 $(obj.nested.child+" :selected").prop('selected',false);
                 $(obj.nested.child+" option").show();
                 if(obj.nested.by) {
                     $(obj.nested.child+" option[data-"+obj.nested.by+"!='"+$(elem).val()+"']").hide();
+                    console.log('Hiding '+obj.nested.child+" option[data-"+obj.nested.by+"!='"+$(elem).val()+"']");
                 }
                 $(obj.nested.child+" option[value='0']").show();
                 $(obj.nested.child).trigger('change');
@@ -72,4 +93,6 @@ $.fn.scapo = function(obj){
 
         }
     });
+    console.log("---");
+
 };
